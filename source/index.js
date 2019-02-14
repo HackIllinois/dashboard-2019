8cd4105555fde321e57b0600d794eadf8b2ab591
Jonathan Xu <jonathanj.xu97@gmail.com>
2019-02-14 15:07:10 -0600

    Time display

--- source/index.js
+++ source/index.js
@@ -1,8 +1,12 @@
+import $ from 'jquery';
+
 import './styles/reset.css';
 import './styles/normalize.css';
 import './styles/skeleton.css';
 import './styles/dashboard.scss';
 
-const x = 33 * 20;
-
-console.log(`x is ${x}`)
+$(document).ready(() => {
+  const x = 33 * 20;
+  console.log('doc ready');
+  console.log(`x is ${x}`);
+});
