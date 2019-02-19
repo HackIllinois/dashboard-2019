ddbda0a6d5d2b38f660fa97ba4808ac67b4b625d
Jonathan Phipps <jonathanmp25@gmail.com>
2019-02-19 14:45:42 -0600

    set up events (#1)
    * set up events (does not refresh)
    
    * Add notifications

--- source/index.js
+++ source/index.js
@@ -9,4 +9,53 @@ $(document).ready(() => {
   const x = 33 * 20;
   console.log('doc ready');
   console.log(`x is ${x}`);
+
+  // Events
+  $.get('/api/event/', ({ events }) => {
+    const time = new Date().getTime();
+
+    // Sort events chronologically
+    events.sort((a, b) => a.startTime - b.startTime);
+
+    // Get relevent events
+    const cur = [];
+    const upcoming = [];
+    events.forEach((event) => {
+      if (event.startTime <= time && event.endTime > time) {
+        cur.push(event);
+      } else if (event.startTime > time) {
+        upcoming.push(event);
+      }
+    });
+
+    // Add events to dom
+    cur.forEach((event) => {
+      const loc = event.locationDescription;
+      $('#cur-events-cont').append(`
+        <div class="event">
+          <p class="name">${event.name}</p>
+          <p class="location">${loc}</p>
+        </div>
+      `);
+    });
+    upcoming.forEach((event) => {
+      const loc = event.locationDescription;
+      $('#upcoming-events-cont').append(`
+        <div class="event">
+          <p class="name">${event.name}</p>
+          <p class="location">${loc}</p>
+        </div>
+      `);
+    });
+  });
+
+  $.get('/api/notifications/all', ({ notifications }) => {
+    notifications.forEach((el) => {
+      $('#announcements-cont').append(`
+        <div class="event">
+          <p class="name">${notification.title}</p>
+        </div>
+      `);
+    });
+  });
 });
