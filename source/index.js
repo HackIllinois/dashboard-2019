63c7b330e9c6820a0a1c8df2c5dcdc1cde10fb4e
Jonathan Phipps <jonathanmp25@gmail.com>
2019-02-21 21:30:40 -0600

    Make changes

--- source/index.js
+++ source/index.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 import { startCountdown } from './cells/countdown';
 import { init as initSponsors, start as startSponsors } from './cells/sponsors';
 import { startTime } from './cells/time';
+import fetchData from './api/events';
 
 import './styles/reset.css';
 import './styles/normalize.css';
@@ -17,59 +18,3 @@ $(document).ready(() => {
   fetchData();
   setInterval(fetchData, 10000);
 });
-
-const fetchData = () => {
-  // Events
-  $.get('/api/event/', ({ events }) => {
-    // Remove existing events from DOM
-    $('#cur-events-cont').empty();
-    $('#upcoming-events-cont').empty();
-
-    const time = new Date().getTime();
-
-    // Sort events chronologically
-    events.sort((a, b) => a.startTime - b.startTime);
-
-    // Get relevent events
-    const cur = [];
-    const upcoming = [];
-    events.forEach((event) => {
-      if (event.startTime <= time && event.endTime > time) {
-        cur.push(event);
-      } else if (event.startTime > time && upcoming.length < 4) {
-        upcoming.push(event);
-      }
-    });
-
-    // Add events to dom
-    cur.forEach((event) => {
-      const loc = event.locations[0].description;
-      $('#cur-events-cont').append(`
-        <div class="event">
-          <p class="name">${event.name}</p>
-          <p class="location">${loc}</p>
-        </div>
-      `);
-    });
-    upcoming.forEach((event) => {
-      const loc = event.locations[0].description;
-      $('#upcoming-events-cont').append(`
-        <div class="event">
-          <p class="name">${event.name}</p>
-          <p class="location">${loc}</p>
-        </div>
-      `);
-    });
-  });
-
-  $.get('/api/notifications/public/', ({ notifications }) => {
-    if (notifications[0]) {
-      $('#announcements-cont').empty();
-      $('#announcements-cont').append(`
-        <div class="announcement">
-          <p class="name">${notifications[0].title}</p>
-        </div>
-      `);
-    }
-  });
-};
