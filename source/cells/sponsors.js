9eba95c5d4d014a7ffb020937b594ea203f95b5d
Jonathan Xu <jonathanj.xu97@gmail.com>
2019-02-21 21:25:49 -0600

    BP logo, countdown reset

--- source/cells/sponsors.js
+++ source/cells/sponsors.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 
 import Amadeus from '../assets/Sponsors/Amadeus.png';
+import BP from '../assets/Sponsors/BP.png';
 import C1 from '../assets/Sponsors/C1.png';
 import Caterpillar from '../assets/Sponsors/Caterpillar.png';
 import Citadel from '../assets/Sponsors/Citadel.png';
@@ -22,7 +23,7 @@ import TwoSigma from '../assets/Sponsors/TwoSigma.png';
 import Zeit from '../assets/Sponsors/Zeit.svg';
 
 let sponsors = [
-  Amadeus, C1, Caterpillar, Citadel, Facebook, Fulcrum, Google, IMC, Jackson, Mirus,
+  Amadeus, BP, C1, Caterpillar, Citadel, Facebook, Fulcrum, Google, IMC, Jackson, Mirus,
   Mozilla, NPM, Nvisia, Optum, Particle, RC, Schlum, SNL, TwoSigma, Zeit,
 ];
 const order = [6, 2, 4, 1, 7, 8, 5, 0, 3]
